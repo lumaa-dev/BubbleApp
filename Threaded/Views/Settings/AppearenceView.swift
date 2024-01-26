095c811572e9d19c5d861eb73e4269ea67892202
Lumaa <nathan@ama49.fr>
2024-01-26 14:38:43 +0100

    Browser setting in "Appearence"

--- Threaded/Views/Settings/AppearenceView.swift
+++ Threaded/Views/Settings/AppearenceView.swift
@@ -42,6 +42,21 @@ struct AppearenceView: View {
             .pickerStyle(.inline)
             .listRowThreaded()
             
+            Picker(LocalizedStringKey("setting.appearence.browser"), selection: $userPreferences.browserType) {
+                ForEach(UserPreferences.BrowserType.allCases, id: \.self) { type in
+                    switch (type) {
+                        case .inApp:
+                            Text("setting.appearence.browser.in-app")
+                                .tag(UserPreferences.BrowserType.inApp)
+                        case .outApp:
+                            Text("setting.appearence.browser.out-app")
+                                .tag(UserPreferences.BrowserType.outApp)
+                    }
+                }
+            }
+            .pickerStyle(.inline)
+            .listRowThreaded()
+            
             if userPreferences.showExperimental {
                 Section(header: Text("experimental")) {
                     Toggle(LocalizedStringKey("setting.appearence.reply-symbols"), isOn: $userPreferences.experimental.replySymbol)
@@ -54,34 +69,13 @@ struct AppearenceView: View {
         .navigationBarTitleDisplayMode(.inline)
         .navigationBarBackButtonHidden()
         .onAppear {
-            do {
-                let oldPreferences = try UserPreferences.loadAsCurrent() ?? UserPreferences.defaultPreferences
-                
-                userPreferences.displayedName = oldPreferences.displayedName
-                userPreferences.profilePictureShape = oldPreferences.profilePictureShape
-                
-                userPreferences.experimental.replySymbol = oldPreferences.experimental.replySymbol
-            } catch {
-                print(error)
-                dismiss()
-            }
+            loadOld()
         }
         .toolbar {
             ToolbarItem(placement: .cancellationAction) {
                 Button {
-                    do {
-                        let oldPreferences = try UserPreferences.loadAsCurrent() ?? UserPreferences.defaultPreferences
-                        
-                        userPreferences.displayedName = oldPreferences.displayedName
-                        userPreferences.profilePictureShape = oldPreferences.profilePictureShape
-                        
-                        userPreferences.experimental.replySymbol = oldPreferences.experimental.replySymbol
-                        
-                        dismiss()
-                    } catch {
-                        print(error)
-                        dismiss()
-                    }
+                    loadOld()
+                    dismiss()
                 } label: {
                     Text("settings.cancel")
                 }
@@ -99,6 +93,21 @@ struct AppearenceView: View {
                     Text("settings.done")
                 }
             }
+        }
+    }
+    
+    private func loadOld() {
+        do {
+            let oldPreferences = try UserPreferences.loadAsCurrent() ?? UserPreferences.defaultPreferences
+            
+            userPreferences.displayedName = oldPreferences.displayedName
+            userPreferences.profilePictureShape = oldPreferences.profilePictureShape
+            userPreferences.browserType = oldPreferences.browserType
+            
+            userPreferences.experimental.replySymbol = oldPreferences.experimental.replySymbol
+        } catch {
+            print(error)
+            dismiss()
         }
     }
 }
