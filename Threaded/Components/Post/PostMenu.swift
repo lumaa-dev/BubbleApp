3564ceb4703a27a5d5024eb46e5fc070e328ba6b
Lumaa <nathan@ama49.fr>
2024-01-26 14:09:52 +0100

    Copy text of status

--- Threaded/Components/Post/PostMenu.swift
+++ Threaded/Components/Post/PostMenu.swift
@@ -1,29 +1,42 @@
 //Made by Lumaa
 
 import SwiftUI
+import UniformTypeIdentifiers
 
 struct PostMenu: View {
-    @Environment(\.colorScheme) private var colorScheme
     @Environment(Navigator.self) private var navigator
+    @Environment(AccountManager.self) private var accountManager
+    @Environment(\.colorScheme) private var colorScheme
     @Environment(\.displayScale) private var displayScale
     
     var status: Status
     
+    private var isOwner: Bool {
+        if let acc = accountManager.getAccount() {
+            return status.account.id == acc.id
+        }
+        return false
+    }
+    
     var body: some View {
         Menu {
-            Button(role: .destructive) {
-                print("Delete")
-            } label: {
-                Label("status.menu.delete", systemImage: "trash")
+            if isOwner {
+                Button(role: .destructive) {
+                    Task {
+                        await deleteStatus()
+                    }
+                } label: {
+                    Label("status.menu.delete", systemImage: "trash")
+                }
+                
+                Button {
+                    navigator.presentedSheet = .post(content: status.reblogAsAsStatus?.content.asRawText ?? status.content.asRawText, replyId: nil, editId: status.reblogAsAsStatus?.id ?? status.id)
+                } label: {
+                    Label("status.menu.edit", systemImage: "pencil.and.scribble")
+                }
+                
+                Divider()
             }
-            
-            Button {
-                print("Edit")
-            } label: {
-                Label("status.menu.edit", systemImage: "pencil.and.scribble")
-            }
-            
-            Divider()
             
             Menu {
                 ShareLink(item: URL(string: status.url ?? "https://joinmastodon.org/")!) {
@@ -32,10 +45,18 @@ struct PostMenu: View {
                 
                 Button {
                     Task {
-                        await createImage()
+                        createImage()
                     }
                 } label: {
                     Label("status.menu.share-image", systemImage: "photo")
+                }
+                
+                Divider()
+                
+                Button {
+                    UIPasteboard.general.setValue(status.reblogAsAsStatus?.content.asRawText ?? status.content.asRawText, forPasteboardType: UTType.plainText.identifier)
+                } label: {
+                    Label("status.menu.copy-text", systemImage: "list.clipboard")
                 }
             } label: {
                 Label("status.menu.share", systemImage: "paperplane")
@@ -64,7 +85,13 @@ struct PostMenu: View {
         render.isOpaque = false
         
         if let image = render.uiImage {
-            navigator.presentedSheet = .shareImage(image: image)
+            navigator.presentedSheet = .shareImage(image: image, status: status)
+        }
+    }
+    
+    private func deleteStatus() async {
+        if let client = accountManager.getClient() {
+            _ = try? await client.delete(endpoint: Statuses.status(id: status.id))
         }
     }
 }
