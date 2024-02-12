0c69945d50ba5736197f49e29466c06b2c63a286
Lumaa <nathan@ama49.fr>
2024-02-12 12:46:35 +0100

    Features list in Threaded+

--- Threaded/Views/Settings/ShopView.swift
+++ Threaded/Views/Settings/ShopView.swift
@@ -18,6 +18,9 @@ struct ShopView: View {
                 .frame(width: 100, height: 100)
                 .padding(.vertical)
             
+            features
+                .padding(.bottom)
+            
             Spacer()
             
             VStack(spacing: 20) {
@@ -58,6 +61,7 @@ struct ShopView: View {
         }
         .frame(width: delegate.windowWidth)
         .background(Color.appBackground)
+        .navigationTitle(Text(String("Threaded+")))
         .sheet(isPresented: $showSub) {
             ShopView.SubView()
         }
@@ -65,10 +69,73 @@ struct ShopView: View {
             ShopView.LifetimeView()
         }
     }
+    
+    var features: some View {
+        ScrollView(.vertical, showsIndicators: true) {
+            VStack(spacing: 25) {
+                Text("shop.features")
+                    .font(.title.bold())
+                    .scrollTransition { content, phase in
+                        content
+                            .opacity(phase.isIdentity ? 1 : 0)
+                            .blur(radius: phase.isIdentity ? 0 : 5)
+                            .offset(y: phase.isIdentity ? 0 : -15)
+                    }
+                
+                feature("shop.features.drafts", description: "shop.features.drafts.description", systemImage: "pencil.and.outline")
+                
+                feature("shop.features.analytics", description: "shop.features.analytics.description", systemImage: "chart.line.uptrend.xyaxis.circle")
+                
+                feature("shop.features.content-filter", description: "shop.features.content-filter.description", systemImage: "wand.and.stars")
+                
+                feature("shop.features.download-atchmnt", description: "shop.features.download-atchmnt.description", systemImage: "photo.badge.arrow.down")
+                
+                feature("shop.features.vip", description: "shop.features.vip.description", systemImage: "crown")
+            }
+            .frame(width: delegate.windowWidth)
+        }
+        .scrollIndicatorsFlash(onAppear: true)
+        .scrollClipDisabled()
+    }
+    
+    @ViewBuilder
+    private func feature(_ title: LocalizedStringKey, description: LocalizedStringKey = LocalizedStringKey(stringLiteral: ""), systemImage: String) -> some View {
+        HStack(alignment: .center) {
+            Image(systemName: systemImage)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 40, height: 40)
+            
+            VStack(alignment: .leading) {
+                Text(title)
+                    .bold()
+                    .lineLimit(1)
+                    .multilineTextAlignment(.leading)
+                Text(description)
+                    .font(.callout)
+                    .lineLimit(2)
+                    .multilineTextAlignment(.leading)
+            }
+            
+            Spacer()
+        }
+        .padding(.leading, 20)
+        .frame(width: delegate.windowWidth - 30)
+        .padding(.vertical)
+        .background(Color.gray.opacity(0.2))
+        .clipShape(.capsule)
+        .scrollTransition { content, phase in
+            content
+                .opacity(phase.isIdentity ? 1 : 0)
+                .scaleEffect(x: phase.isIdentity ? 1 : 0.5, y: phase.isIdentity ? 1 : 0.75, anchor: .center)
+                .blur(radius: phase.isIdentity ? 0 : 10)
+                .offset(y: phase.isIdentity ? 0 : 10)
+        }
+    }
 }
 
 extension ShopView {
-    private struct SubView: View {
+    struct SubView: View {
         var body: some View {
             NavigationStack {
                 SubscriptionStoreView(productIDs:  ["fr.lumaa.Threaded.Plus.monthly", "fr.lumaa.ThreadedPlus.yearly"]) {
@@ -120,7 +187,7 @@ extension ShopView {
         }
     }
     
-    private struct LifetimeView: View {
+    struct LifetimeView: View {
         var body: some View {
             VStack {
                 Text("shop.threaded-plus.lifetime.header")
@@ -148,4 +215,6 @@ extension ShopView {
 
 #Preview {
     ShopView()
+        .environment(AppDelegate())
+//        .environment(\.locale, Locale(identifier: "en-us"))
 }
