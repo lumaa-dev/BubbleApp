3156877eff4a74dbc24a951685d87ce78e3977a4
Lumaa <lumaa@lumaa.fr>
2024-03-23 09:48:29 +0100

    Few improvements for ComingSoon

--- Threaded/Components/ComingSoonView.swift
+++ Threaded/Components/ComingSoonView.swift
@@ -3,23 +3,35 @@
 import SwiftUI
 
 struct ComingSoonView: View {
+    init() {}
+    
     @State private var spin: CGFloat = 0
     
     var body: some View {
         VStack(spacing: 5) {
             Text(String("👀"))
                 .font(.system(size: 62))
-                .rotation3DEffect(.degrees(spin), axis: (x: 0, y: 1, z: 0))
-                .onTapGesture {
-                    withAnimation(.spring.speed(0.8)) {
-                        spin = 360
-                    }
-                    spin = 0
-                }
+                .padding()
+                .background(Color.gray.opacity(0.3))
+                .clipShape(Circle())
+                .tapToSpin(spin: $spin)
             
             Text("coming-soon")
                 .font(.title.bold())
         }
+    }
+}
+
+extension View {
+    func tapToSpin(spin: Binding<CGFloat>, defaultValue: CGFloat = 0) -> some View {
+        self
+            .rotation3DEffect(.degrees(spin.wrappedValue), axis: (x: 0, y: 1, z: 0))
+            .onTapGesture {
+                withAnimation(.spring.speed(0.8)) {
+                    spin.wrappedValue = 360
+                }
+                spin.wrappedValue = defaultValue
+            }
     }
 }
 
---
+++ Threaded/Views/Settings/RestrictedView.swift
@@ -0,0 +1,13 @@
+//Made by Lumaa
+
+import SwiftUI
+
+struct RestrictedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RestrictedView()
+}
