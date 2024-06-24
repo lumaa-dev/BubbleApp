e0833cebe8f8d7ae338c3d6792aa87ebce4c1454
Lumaa <lumaa@lumaa.fr>
2024-06-24 16:12:30 +0200

    Cross post to alt

--- Threaded/Components/Post/PostMenu.swift
+++ Threaded/Components/Post/PostMenu.swift
@@ -6,6 +6,7 @@ import UniformTypeIdentifiers
 struct PostMenu: View {
     @Environment(UniversalNavigator.self) private var navigator
     @Environment(AccountManager.self) private var accountManager
+    @Environment(\.openURL) private var openURL
     @Environment(\.dismiss) private var dismiss
     @Environment(\.colorScheme) private var colorScheme
     @Environment(\.displayScale) private var displayScale
@@ -37,6 +38,30 @@ struct PostMenu: View {
                     navigator.presentedSheet = .post(content: status.reblogAsAsStatus?.content.asRawText ?? status.content.asRawText, replyId: nil, editId: status.reblogAsAsStatus?.id ?? status.id)
                 } label: {
                     Label("status.menu.edit", systemImage: "pencil.and.scribble")
+                }
+                
+                Divider()
+                
+                Menu {
+                    Button {
+                        openURL(URL(string: AltClients.IvoryApp.createPost(status.reblogAsAsStatus?.content.asRawText ?? status.content.asRawText))!)
+                    } label: {
+                        Text(AltClients.IvoryApp.name)
+                    }
+                    
+                    Button {
+                        openURL(URL(string: AltClients.ThreadsApp.createPost(status.reblogAsAsStatus?.content.asRawText ?? status.content.asRawText))!)
+                    } label: {
+                        Text(AltClients.ThreadsApp.name)
+                    }
+                    
+                    Button {
+                        openURL(URL(string: AltClients.XApp.createPost(status.reblogAsAsStatus?.content.asRawText ?? status.content.asRawText))!)
+                    } label: {
+                        Text(AltClients.XApp.name)
+                    }
+                } label: {
+                    Label("status.cross-post.alts", systemImage: "shuffle")
                 }
                 
                 Divider()
