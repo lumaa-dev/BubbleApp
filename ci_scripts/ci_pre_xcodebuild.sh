3440fb7b70bef80c0e916708816ce6bec1f84767
lumaa-dev <lumaa@lumaa.fr>
2025-06-17 14:37:59 +0200

    change dir var

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -3,27 +3,27 @@
 
 echo "🔧 Preparing confidential files for build..."
 
-mkdir -p "$PROJECT_DIR/Store"
+mkdir -p "$CI_PRIMARY_REPOSITORY_PATH/Store"
 
 # Check and decode SecretPlist
 if [[ -z "${SecretPlist:-}" ]]; then
   echo "❌ Missing SecretPlist environment variable"
   exit 1
 fi
-echo "$SecretPlist" | base64 -d > "$PROJECT_DIR/Secret.plist"
+echo "$SecretPlist" | base64 -d > "$CI_PRIMARY_REPOSITORY_PATH/Secret.plist"
 
 # Check and decode StoreKitConfig
 if [[ -z "${StoreKitConfig:-}" ]]; then
   echo "❌ Missing StoreKitConfig environment variable"
   exit 1
 fi
-echo "$StoreKitConfig" | base64 -d > "$PROJECT_DIR/Store/BubblePlus.storekit"
+echo "$StoreKitConfig" | base64 -d > "$CI_PRIMARY_REPOSITORY_PATH/Store/BubblePlus.storekit"
 
 # Check and decode StoreKitCertificate
 if [[ -z "${StoreKitCertificate:-}" ]]; then
   echo "❌ Missing StoreKitCertificate environment variable"
   exit 1
 fi
-echo "$StoreKitCertificate" | base64 -d > "$PROJECT_DIR/Store/StoreKitTestCertificate.cer"
+echo "$StoreKitCertificate" | base64 -d > "$CI_PRIMARY_REPOSITORY_PATH/Store/StoreKitTestCertificate.cer"
 
-echo "✅ All secret files restored to $PROJECT_DIR/Configs/"
+echo "✅ All secret files restored to $CI_PRIMARY_REPOSITORY_PATH"
