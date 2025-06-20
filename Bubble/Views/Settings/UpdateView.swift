61856b08a9dadfa35b24cc0ed97967c7d54a1c75
lumaa-dev <lumaa@lumaa.fr>
2025-06-20 14:29:41 +0200

    v2.0.0 update sheet

--- Bubble/Views/Settings/UpdateView.swift
+++ Bubble/Views/Settings/UpdateView.swift
@@ -39,67 +39,20 @@ struct UpdateView: View {
     
     var features: some View {
         VStack(spacing: 60) {
-            newFeature(image: "HeroIcon", title: "Bubble+ is fixed!", text: "Bubble+ is back for everyone who ever wants to buy it.")
+            newFeature(image: Image("HeroIcon"), title: "New Bubble", text: "Bubble receives a brand new look, as well as iOS 26's Liquid Glass.")
 
-            newFeature(systemImage: "capsule", title: "Bubble v2.0.0", text: "Bubble is having its biggest rework since its release in July 2024")
+            newFeature(image: Image(systemName: "apple.intelligence"), title: "(SOON) Less thinking for more", text: "Apple Intelligence can help you write and reply to posts")
 
-            newFeature(systemImage: "brain.filled.head.profile", title: "Active Brain", text: "Lumaa's been working on other fantastic projects like Cider Remote lately... Check them out!")
+            newFeature(image: Image(systemName: "beziercurve"), title: "Microdata Refinement", text: "Bubble has been largely optimized so that it runs smoother than ever")
         }
         .frame(height: 500)
     }
 
     @ViewBuilder
-    private func newFeature(systemImage: String, title: String, text: String) -> some View {
+    private func newFeature(image: Image, title: String, text: String) -> some View {
         ViewThatFits {
             HStack(alignment: .center) {
-                Image(systemName: systemImage)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 60, height: 60)
-                    .foregroundStyle(Color(uiColor: UIColor.label))
-                
-                Spacer()
-                    .frame(width: 30)
-                
-                VStack(alignment: .leading) {
-                    Text(title)
-                        .bold()
-                        .foregroundStyle(Color(uiColor: UIColor.label))
-                        .multilineTextAlignment(.leading)
-                        .lineLimit(1)
-                    
-                    Text(text)
-                        .foregroundStyle(Color(uiColor: UIColor.label).opacity(0.7))
-                        .font(.callout)
-                        .multilineTextAlignment(.leading)
-                        .lineLimit(4)
-                }
-                
-                Spacer()
-            }
-            .frame(width: 330)
-            
-            VStack(alignment: .leading) {
-                Text(title)
-                    .bold()
-                    .foregroundStyle(Color(uiColor: UIColor.label))
-                    .multilineTextAlignment(.leading)
-                    .lineLimit(1)
-                
-                Text(text)
-                    .foregroundStyle(Color(uiColor: UIColor.label))
-                    .multilineTextAlignment(.leading)
-                    .lineLimit(4)
-            }
-            .padding(.horizontal)
-        }
-    }
-    
-    @ViewBuilder
-    private func newFeature(image: String, title: String, text: String) -> some View {
-        ViewThatFits {
-            HStack(alignment: .center) {
-                Image(image)
+                image
                     .resizable()
                     .scaledToFit()
                     .frame(width: 60, height: 60)
@@ -145,7 +98,7 @@ struct UpdateView: View {
     @ViewBuilder
     private func newFeature(_ feature: AppInfo.Feature) -> some View {
         self.newFeature(
-            systemImage: feature.details.systemImage,
+            image: Image(systemName: feature.details.systemImage),
             title: feature.details.title.toString(),
             text: feature.details.description.toString()
         )
